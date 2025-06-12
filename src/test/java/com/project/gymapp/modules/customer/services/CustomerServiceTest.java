458bdffcdd27195f9aa05d600c20d14f5f736e0c
eoliveirasobrinho <e.oliveira.sobrinho1987@gmail.com>
2025-06-12 16:04:07 -0300

    beginning of test service

---
+++ src/test/java/com/project/gymapp/modules/customer/services/CustomerServiceTest.java
@@ -0,0 +1,39 @@
+package com.project.gymapp.modules.customer.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Description;
+
+public class CustomerServiceTest {
+
+    @Test
+    @Description("Should create a customer")
+    void testShouldCreateCustomer() {
+
+    }
+
+    @Test
+    @Description("Should not create a customer")
+    void testShouldNotCreateCsutomer() {
+
+    }
+
+    @Test
+    void testDeleteCustomer() {
+
+    }
+
+    @Test
+    void testGetAllCustomers() {
+
+    }
+
+    @Test
+    void testGetCustomerById() {
+
+    }
+
+    @Test
+    void testUpdateCustomer() {
+
+    }
+}
